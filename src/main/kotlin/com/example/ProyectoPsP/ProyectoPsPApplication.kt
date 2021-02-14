79603cdc666f4b6c560d92078781e13ae63b0fe8
ceaq_ <ceaq.5.3@gmail.com>
2021-02-14 23:50:32 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/ProyectoPsP/ProyectoPsPApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ProyectoPsP
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectoPsPApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectoPsPApplication>(*args)
+}
